301b431f61e8c9a966ef5296b243c1981b64537b
Emmanuel-Youpy <emmydeji58@gmail.com>
2022-06-29 08:16:35 -0400

    Components

--- src/index.js
+++ src/index.js
@@ -5,6 +5,26 @@ import App from './App'
 
 import './api/server'
 
+import store from './store'
+
+store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
+
+console.log('State after dispatch: ', store.getState())
+
+console.log('Dispatching action')
+store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
+console.log('Dispatch complete')
+
+console.log('Initial state: ', store.getState())
+
+const unsubscribe = store.subscribe(() =>
+  console.log('State after dispatch: ', store.getState())
+)
+
+store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
+
+unsubscribe()
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
---
+++ src/store.js
@@ -0,0 +1,23 @@
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './reducer'
+import {
+  sayHiOnDispatch,
+  includeMeaningOfLife,
+} from './exampleAddons/enhancers'
+import { print1, print2, print3 } from './exampleAddons/middleware'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+const middlewareEnhancer = applyMiddleware(print1, print2, print3)
+const composedEnhancer = composeWithDevTools(
+  // EXAMPLE: Add whatever middleware you actually want to use here
+  applyMiddleware(print1, print2, print3)
+  // other store enhancers if any
+)
+
+const store = createStore(rootReducer, composedEnhancer)
+
+// const composedEnhancer = compose(sayHiOnDispatch, includeMeaningOfLife)
+
+// const store = createStore(rootReducer, undefined, composedEnhancer)
+
+export default store
